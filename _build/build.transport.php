281f3999b54555999e93b53f143121a4a3699bce
Steve Hamblett <steve.hamblett@linux.com>
2010-08-13 08:34:47 +0100

    Updates for issues 3,4,2,1, see the changelog for details. Version and documentation updated for release 1.6 beta
    Signed-off-by: Steve Hamblett <steve.hamblett@linux.com>

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -27,7 +27,7 @@ echo '<pre>'; $modx->setLogTarget('ECHO');
 error_reporting(E_ALL); ini_set('display_errors', true);
 
 $name = 'provisioner';
-$version = '1.5';
+$version = '1.6';
 $release = 'beta';
 
 $modx->loadClass('transport.modPackageBuilder', '', false, true);
