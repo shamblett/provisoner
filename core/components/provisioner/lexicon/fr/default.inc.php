b8a5cec997396db47b2dd9cbd0c3ab1e7791cabb
Romain <romain@meltingmedia.net>
2010-11-28 01:57:09 +0100

    added french translation

---
+++ core/components/provisioner/lexicon/fr/default.inc.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * Provisioner lexicon
+ *
+ * @category   Provisioning
+ * @author     S. Hamblett <steve.hamblett@linux.com>
+ * @copyright  2009 S. Hamblett
+ * @license    GPLv3 http://www.gnu.org/licenses/gpl.html
+ * @link       none
+ * @language   fr
+ *
+ * @package    provisioner
+ * @subpackage lexicon
+ */
+$_lang['provisioner'] = 'Provisioner';
+$_lang['button_go'] = 'Valider';
+$_lang['menu_account'] = 'Utilisateur';
+$_lang['menu_provisioner'] = 'Provisioner';
+$_lang['menu_administration'] = 'Serveur distant';
+$_lang['menu_administration_tab'] = 'Administration';
+$_lang['menu_resources'] = 'Ressources';
+$_lang['menu_password'] = 'Mot de passe';
+$_lang['menu_elements'] = 'Éléments';
+$_lang['menu_files'] = 'Fichiers';
+$_lang['menu_url'] = 'URL';
+$_lang['noaccount_error'] = 'Aucun compte spécifié';
+$_lang['nopassword_error'] = 'Aucun mot de passe spécifié';
+$_lang['nourl_error'] = 'Aucune URL de connecteur spécifiée';
+$_lang['loginsuccess'] = 'Connexion établie';
+$_lang['loginfailed'] = 'Connexion échouée';
+$_lang['logoutsuccess'] = 'Déconnexion effectuée';
+$_lang['logoutfailed'] = 'Déconnexion échouée';
+$_lang['wronguser'] = 'Utilisateur déjà connecté';
+$_lang['radio_login'] = 'Login';
+$_lang['radio_logout'] = 'Logout';
+$_lang['notloggedin'] = 'Non connecté';
+$_lang['urlnotfound'] = 'URL de connecteur invalide';
+$_lang['unknownerror'] = 'Erreur inconnue';
+$_lang['pv_resources'] = 'Ressources distantes';
+$_lang['resources_desc'] = 'Ressources se trouvant sur le site distant';
+$_lang['menu_resources_tab'] = 'Ressources';
+$_lang['getresourcesfailed'] = 'Chargement des ressources distantes échoué';
+$_lang['resource_menu'] = 'Ressources distantes';
+$_lang['import_resource'] = 'Importer';
+$_lang['importresourcesfailed'] = 'Import des ressources échoué';
+$_lang['importsuccess'] = 'Importation effectué avec succès';
+$_lang['invalidresourceid'] = 'ID de ressource non valide';
+$_lang['failedtogetremoteresource'] = 'Impossible de charger les ressources distantes';
+$_lang['failedtocreatelocalresource'] = 'Impossible de créer la ressource localement';
+$_lang['menu_elements_tab'] = 'Éléments';
+$_lang['pv_elements'] = 'Éléments distants';
+$_lang['elements_desc'] = 'Éléments présents sur le site distant';
+$_lang['getelementsfailed'] = 'Chargement des éléments échoué';
+$_lang['import_element'] = 'Importer';
+$_lang['importelementsfailed'] = 'Import des éléments distants échoué';
+$_lang['failedtocreatelocalelement'] = 'Création d\'élément échouée';
+$_lang['notyetimplemented'] = 'Fonction non implémentée - désolé!';
+$_lang['menu_files_tab'] = 'Fichiers';
+$_lang['pv_files'] = 'Fichiers distants';
+$_lang['files_desc'] = 'Fichiers présents sur le site distant';
+$_lang['getfilesfailed'] = 'Chargement des fichiers échoué';
+$_lang['import_file'] = 'Importer';
+$_lang['nofilename'] = 'Nom de fichier non spécifié';
+$_lang['nopathname'] = 'Chemon d\'accès non spéficié';
+$_lang['menu_packages_tab'] = 'Paquets';
+$_lang['packages'] = 'Paquets distants';
+$_lang['packages_desc'] = 'Paquets installés sur le site distant';
+$_lang['getpackagesfailed'] = 'Chargement du paquet échoué';
+$_lang['package_signature'] = 'Nom du paquet';
+$_lang['localinstall'] = 'Installés localement';
+$_lang['local_installed'] = 'oui';
+$_lang['not_local_installed'] = 'non';
+$_lang['menu_users_tab'] = 'Utilisateurs';
+$_lang['users'] = 'Utilisateurs distants';
+$_lang['users_desc'] = 'Les utilisateurs existant sur le site distant';
+$_lang['getusersfailed'] = 'Chargement de l\'utilisateur échoué';
+$_lang['user_full_name'] = 'Nom complet';
+$_lang['user_block'] = 'Utilisateur bloqué';
+$_lang['import_user'] = 'Importer';
+$_lang['importusersfailed'] = 'Import de l\'utlisateur échoué';
+$_lang['invaliduserid'] = 'ID d\'utilisateur invalide';
+$_lang['failedtocreatelocaluser'] = 'Impossible de créer l\'utilisateur localement';
+$_lang['failedtocreatelocaluserprofile'] = 'Impossible de créer le profil d\'utilisateur localement';
+$_lang['failedtocreatelocalfile'] = 'Impossible de créer le fichier localement';
+$_lang['failedtocreatelocalfolder'] = 'Impossible de créer le répertoire localement';
+$_lang['provisioner.desc'] = 'Composant permettant d\'importer des données depuis un site Revolution ou Evolution';
+$_lang['importfilesfailed'] = 'Import échoué des fichiers : ';
+/* 1.0 GA additions */
+$_lang['revolution'] = 'Revolution';
+$_lang['evolution'] = 'Evolution';
+$_lang['remotesite'] = 'Site distant : ';
+$_lang['adminaction'] = 'Action';
+$_lang['import_convert_resource'] = 'Importation Conversion Tags';
+$_lang['account_details'] = 'Mot de passe et URL du site distant';
+$_lang['remote_site'] = 'Version de MODx du site distant';
+$_lang['admin_action'] = 'Action';
+$_lang['already_imported'] = ': déjà importés?';
+$_lang['user_type'] = 'Type';
+$_lang['user_email'] = 'E-Mail';
+$_lang['importfoldernotwriteable'] = 'Répertoire d\'import non inscriptible';
+$_lang['importfilenolength'] = 'Le fichier importé est null (0 octet)';
+$_lang['admin_remember'] = 'Mémoriser…';
+$_lang['nositeid_error'] = 'ID du site distant non spécifié';
+$_lang['menu_siteid'] = 'ID du site distant';
+$_lang['nocurl'] = 'Oups! L\'extension PHP cURL ne semble pas installée, veuillez l\'installer';
+$_lang['norevogateway'] = 'Le paquet "Passerelle-Revolution" n\'est pas présent sur le site Evolution distant';
+$_lang['admin_helptext'] = "<p>Pour les sites sous MODx Revolution, l'URL est l'URL du répertoire /connectors.
+                Pour les sites sous MODx Evolution, c'est l'URL du site lui même.</p><p>Pour Evolution, 
+                le compte utilisateur doit être un compte administrateur. Pour Revolution, le compte utilisateur 
+                doit avoir des droits suffisants pour « afficher les opérationé ».</p>
+                <p>Pour Revolution, vous devez indiquer l'ID du site.</p>
+                <p>Pour tout problème, veuillez consulter la documentation qui se trouve dans le répertoire<br />
+                /assets/components/provisioner/docs</p>";
